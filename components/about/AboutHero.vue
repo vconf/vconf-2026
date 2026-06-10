eb2eb02d8b4cd5b4760602e52177882a121141af
Ling,Jun- Hao <Antonio.Ling@hepiuscare.com.tw>
2026-06-10 19:37:05 +0800

    fix: 避免 about hero 字體 z-index 優先級高於導覽列

--- components/about/AboutHero.vue
+++ components/about/AboutHero.vue
@@ -2,7 +2,7 @@
   <section class="relative overflow-hidden overflow-x-clip text-clip pb-[266px] pt-[238px] text-center md:pb-[288px] md:pt-[166px]">
     <!-- 切換按鈕 -->
     <ShareThemeToggle class="left-[17px] top-[66px]" />
-    <h1 class="relative z-40 flex justify-center font-sans font-semibold leading-[1] text-vconf-primary">
+    <h1 class="relative flex justify-center font-sans font-semibold leading-[1] text-vconf-primary">
       <span class="pr-[7px] text-[clamp(48px,calc(-5.44px+9.32vw),106px)] leading-[-2%] text-vconf-heading md:pr-[17px]">About</span>
       <span class="pr-[2px] text-[clamp(48px,calc(-5.44px+9.32vw),106px)] leading-[-2%] md:pr-4">VCONF</span>
       <span class="text-[clamp(24px,calc(5.26px+4.662vw),53px)] tracking-[0em]">TW</span>
