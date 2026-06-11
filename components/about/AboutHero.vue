4cca6826bf39938c44880923d15c583abd4381cf
Ling,Jun- Hao <Antonio.Ling@hepiuscare.com.tw>
2026-06-11 22:24:56 +0800

    feat: 調整導覽列不要 sticky 到頂部

--- components/about/AboutHero.vue
+++ components/about/AboutHero.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="relative overflow-hidden overflow-x-clip text-clip pb-[266px] pt-[238px] text-center md:pb-[288px] md:pt-[166px]">
     <!-- 切換按鈕 -->
-    <ShareThemeToggle class="left-[17px] top-[66px]" />
+    <ShareThemeToggle class="left-[17px] top-[66px] z-40" />
     <h1 class="relative flex justify-center font-sans font-semibold leading-[1] text-vconf-primary">
       <span class="pr-[7px] text-[clamp(48px,calc(-5.44px+9.32vw),106px)] leading-[-2%] text-vconf-heading md:pr-[17px]">About</span>
       <span class="pr-[2px] text-[clamp(48px,calc(-5.44px+9.32vw),106px)] leading-[-2%] md:pr-4">VCONF</span>
