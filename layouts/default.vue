36d835867e5aacd73581b15e5fc7f9689b6f1293
Ling,Jun- Hao <Antonio.Ling@hepiuscare.com.tw>
2026-06-13 21:08:08 +0800

    feat: 隨機讓使用者用 light 或 dark theme

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+onMounted(() => {
+  const colorMode = useColorMode()
+  colorMode.preference = Math.random() > 0.5 ? 'light' : 'dark'
+})
+</script>
+
 <template>
   <div>
     <ShareNavBar class="relative z-20" />
