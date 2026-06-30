db6325498984b1b0c6bc8d306284e6f9ee14cc26
Ling,Jun- Hao <Antonio.Ling@hepiuscare.com.tw>
2026-06-30 20:14:18 +0800

    chore: 移除未使用 umami custom event 測試程式碼

--- pages/about.vue
+++ pages/about.vue
@@ -1,14 +1,8 @@
 <script lang="ts" setup>
-import { trackAdImpression } from '@/utils/adTracking'
-
 useSeoMeta({
   title: '關於我們',
   description:
     'v-conf Taiwan 2026 是台灣 Vue.js 開發者的年度聚會，在這裡回顧技術演進、認識同路人，一起想像前端的下一步。',
-})
-
-onMounted(() => {
-  trackAdImpression('testUse')
 })
 </script>
 
